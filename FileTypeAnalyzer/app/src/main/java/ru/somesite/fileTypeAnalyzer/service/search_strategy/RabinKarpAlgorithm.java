c6a338bd6fea7a6e4f9d46f4477def201780bf96
yurinvv <93970925+yurinvv@users.noreply.github.com>
2022-07-01 00:19:39 +0300

    Formula

--- FileTypeAnalyzer/app/src/main/java/ru/somesite/fileTypeAnalyzer/service/search_strategy/RabinKarpAlgorithm.java
+++ FileTypeAnalyzer/app/src/main/java/ru/somesite/fileTypeAnalyzer/service/search_strategy/RabinKarpAlgorithm.java
@@ -1,19 +1,40 @@
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * 1) Calculate the hash value for a pattern.
+ * 2) Moving along the text (from right to left), calculate the
+ * hash value for the current substring of the text using the rolling hash property.
+ * 3) If hash values for the pattern and the current substring are not equal,
+ * move to the next substring. Otherwise, perform a symbol-by-symbol comparison
+ * of the strings. If the strings are indeed equal, add the current index to a list of occurrences.
+ * 4) Repeat steps 2-3 until all substrings of the text are processed. Then, return a list of all occurrences.
+ */
+
 public class RabinKarpAlgorithm implements SearchMethod {
 
     @Override
     public int search(String text, String pattern) {
-        return 0;
+        if (pattern.length() <= text.length()) {
+            int patternHash = polynomialHashing(pattern);
+            int substringHash = polynomialHashing(text.substring(0, pattern.length()));
+
+            for (int i = text.length() - 1; i >= pattern.length() - 1; i--) {
+
+            }
+        }
+        return -1;
     }
+
+    //Hp(Si * S(j+1) * ... * S(j-1)) = ( (Hp( S(i+1) * S(i+2) * ... * Sj ) - Sj * a ^ (j-i-1) ) * a + Si) % m
+    //Rolling hashing
 
     // Hp(s) = (s0 * a^0 + s1 * a^1 + ... + s(n-1) * a^(n-1)) mod m
     // Hp(s) = (s0 * a^0 + s1 * a^1 + ... + s(n-1) * a^(n-1)) % m
     // Here, a is a constant, usually a prime number approximately
     // equal to the total number of different symbols in the alphabet;
     // m is a constant as well, usually a big prime number.
-    public int PolynomialHashing(String string) {
+    public int polynomialHashing(String string) {
         int a = findNumberA(string);
         int m = findNumberM(string);
         int hashCode = 0;
